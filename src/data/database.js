b6f7f15258471513460f7295f4316bd2a0b92591
Andrew Wells <andrewwells96@gmail.com>
2024-04-19 15:02:53 -0400

    entering facts about egypt

--- src/data/database.js
+++ src/data/database.js
@@ -1,6 +1,22 @@
 export const database = {
-  italy: [],
-  egypt: [],
-  scotland: [],
-  canada: [],
+  italy: {},
+  egypt: {
+
+    city: {name: "Cairo", image: "https://media.cntraveler.com/photos/655cdf1d2d09a7e0b27741b5/16:9/w_1920,c_limit/Cairo%20Egypt_GettyImages-1370918272.jpg"},
+    landmark: {name: "The Great Pyramid of Giza", image: "TODO"},
+    citizen: {name: "Mohamed Salah", image: "TODO", bio: "Professional Soccer player",},
+    historicalFacts: {bio: "In the year 48 BC, an accidental fire at the then-acclaimed Library of Alexandria lead to the burning and destruction of over half-a-million scrolls and texts. This was one of the most disastrous events regarding humanity's journey in understanding and knowledge - so disastrous that some historians believe this set humanity back hundreds of years.", image: "https://img.thedailybeast.com/image/upload/c_crop,d_placeholder_euli9k,h_1440,w_2560,x_0,y_0/dpr_1.5/c_limit,w_1044/fl_lossy,q_auto/v1531533951/180714-mcnearney-lost-masterpiece-hero_lxmbyx",},
+
+  // id: number; 
+  // description: string; 
+  // details: string; 
+  // mainImage: string; 
+  // foodImage: string; 
+  // landmarkImage: string;
+  // historicalFacts: string[]; 
+  // citizen: {name: string, details: string, country: string}
+
+  },
+  scotland: {},
+  canada: {},
 };
